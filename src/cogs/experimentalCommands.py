bd498afe6a3ef8b0774140dc46ef85a80b83dd60
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2021-04-23 12:43:26 +0530

    upgrading to embed

--- src/cogs/experimentalCommands.py
+++ src/cogs/experimentalCommands.py
@@ -16,31 +16,31 @@ from src.utils import askme
 
 
 class ExperimentalCommands(commands.Cog, name="Experimetal Commands"):
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 
-    @commands.command(name="cybel", aliases=["c"])
-    async def cybel(self, ctx, *args):
-        """ AI Enabled Chatbot
-        
-        commands: !cybel
-        aliase command: !c
-        
-        Example:
-        input: !cybel Hello
-        cybel: Hi
-        -------------------
-        input: !cybel What is value of PI?
-        cybel: Pi = 3.14
-        """
-        async with ctx.typing():
-            user_question = ' '.join(args)
-            try:
-                bot_response = askme.askMe(user_question)
-                await ctx.send(bot_response)
-            except Exception as e:
-                await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+	@commands.command(name="cybel", aliases=["c"])
+	async def cybel(self, ctx, *args):
+		""" AI Enabled Chatbot
+
+		commands: !cybel
+		aliase command: !c
+
+		Example:
+		input: !cybel Hello
+		cybel: Hi
+		-------------------
+		input: !cybel What is value of PI?
+		cybel: Pi = 3.14
+		"""
+		async with ctx.typing():
+			user_question = ' '.join(args)
+			try:
+				bot_response = askme.askMe(user_question)
+				await ctx.send(bot_response)
+			except Exception as e:
+				await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
 
 
 def setup(bot: commands.Cog):
-    bot.add_cog(ExperimentalCommands(bot))
+	bot.add_cog(ExperimentalCommands(bot))
--- src/utils/askme.py
+++ src/utils/askme.py
@@ -16,18 +16,18 @@ BOTNAME = utils.BOTNAME
 def askMe(question):
 	chatbot = ChatBot(BOTNAME,
 		logic_adapters=[
-        {	
+		{
 			'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'I am sorry, but I do not understand. Please can you write full sentense\nExample:- I am good instead of only good',
-            'maximum_similarity_threshold': 0.8,
-        },
+			'default_response': 'I am sorry, but I do not understand. Please can you write full sentense\nExample:- I am good instead of only good',
+			'maximum_similarity_threshold': 0.8,
+		},
 			'chatterbot.logic.MathematicalEvaluation',
 		],
 		preprocessors = [
 			"chatterbot.preprocessors.clean_whitespace",
 		],
 		input_adaptor="chatterbot.input.TerminalAdaptor",
-        output_adaptor="chatterbot.output.TerminalAdaptor",
+		output_adaptor="chatterbot.output.TerminalAdaptor",
 		database_uri='sqlite:///database.sqlite3')
 	'''
 	trainer = ChatterBotCorpusTrainer(chatbot)
